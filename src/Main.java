2a976a57c77ca6b532497f5bf2b061769bc0a185
EllieWu <a0981262519@gmail.com>
2017-11-13 15:29:24 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int i;
+        i=in.nextInt();
+        for(i=1;i<=7;i+=1)
+            System.out.println(i);
+
+    }
+}
+
